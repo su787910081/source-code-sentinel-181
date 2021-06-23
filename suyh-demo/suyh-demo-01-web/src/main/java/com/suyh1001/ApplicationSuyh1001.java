44f8f06426946ec19deaae1b361eb7f8de64483e
苏云弘 <su_787910081@sina.com>
2021-06-23 21:46:30 +0800

    网关流控-demo

---
+++ suyh-demo/suyh-demo-01-web/src/main/java/com/suyh1001/ApplicationSuyh1001.java
@@ -0,0 +1,11 @@
+package com.suyh1001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationSuyh1001 {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationSuyh1001.class, args);
+    }
+}
---
+++ suyh-demo/suyh-demo-01-web/src/main/java/com/suyh1001/controller/SuyhController.java
@@ -0,0 +1,16 @@
+package com.suyh1001.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/suyh")
+@Slf4j
+public class SuyhController {
+    @RequestMapping("/name")
+    public String name() {
+        log.info("name");
+        return "suyh";
+    }
+}
