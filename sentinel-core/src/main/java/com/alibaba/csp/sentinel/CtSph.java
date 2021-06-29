6a469d2d988c02d2a26aa2d71cb6158b9af9d874
su787910081 <su_787910081@sina.com>
2021-06-29 17:06:29 +0800

    Update CtSph.java

--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/CtSph.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/CtSph.java
@@ -121,31 +121,41 @@ public class CtSph implements Sph {
             // The {@link NullContext} indicates that the amount of context has exceeded the threshold,
             // so here init the entry only. No rule checking will be done.
             // suyh - 对参数和全局配置项做检测，如果不符合要求就直接返回了一个CtEntry对象，不会再进行后面的限流检测，否则进入下面的检测流程。
+            // suyh - 如果是 NullContext, 那么说明context name 超过了2000个，参见ContextUtil#trueEnter
+            // suyh - 这个时候，Sentinel 不再接受处理新的context 配置，也就是不做这些新的接口的统计、限流熔断等
             return new CtEntry(resourceWrapper, null, context);
         }
 
         if (context == null) {
-            // suyh - 历史可能是调用 MyContextUtil.myEnter(...);
             // Using default context.
+            // suyh - 历史可能是调用 MyContextUtil.myEnter(...);
+            // suyh - 前面没有显示调用ContextUtil#enter 方法，这里就会进入到默认的context 中
             context = InternalContextUtil.internalEnter(Constants.CONTEXT_DEFAULT_NAME);
         }
 
         // Global switch is close, no rule checking will do.
+        // suyh - Sentinel 的全局开关，Sentinel 提供了接口让用户可以在dashboard 开启/关闭
         if (!Constants.ON) {
             return new CtEntry(resourceWrapper, null, context);
         }
 
         // 获取该资源对应的SlotChain
+        // suyh - 设计模式中的责任链模式。
+        // suyh - 下面这行代码用于构建 一个责任链，入参是resource，前面我们说过资源的唯一标识 是resource name。
         ProcessorSlot<Object> chain = lookProcessChain(resourceWrapper);
 
         /*
          * Means amount of resources (slot chain) exceeds {@link Constants.MAX_SLOT_CHAIN_SIZE},
          * so no rule checking will be done.
          */
+        // suyh - 根据lookProcessChain方法，我们知道，当resource 超过Constants.MAX_SLOT_CHAIN_SIZE,
+        // suyh - 也就是6000 的时候，Sentinel 开始不处理新的请求，这么做主要是为了Sentinel 的性能考虑。
         if (chain == null) {
             return new CtEntry(resourceWrapper, null, context);
         }
 
+        // suyh - 执行这个责任链。如果抛出BlockException，说明链上的某一环拒绝了该请求，
+        // suyh - 把这个异常往上层业务层抛，业务层处理BlockException 应该进入到熔断降级逻辑处理中。
         Entry e = new CtEntry(resourceWrapper, chain, context);
         try {
             chain.entry(context, resourceWrapper, null, count, prioritized, args);
@@ -194,6 +204,8 @@ public class CtSph implements Sph {
      * @param resourceWrapper target resource
      * @return {@link ProcessorSlotChain} of the resource
      */
+    // suyh - 相同的资源使用的是相同的责任链。
+    // suyh - 默认的责任链可以查看：sentinel-core 模块中的 META-INF/services/com.alibaba.csp.sentinel.slotchain.ProcessorSlot 中的配置
     ProcessorSlot<Object> lookProcessChain(ResourceWrapper resourceWrapper) {
         ProcessorSlotChain chain = chainMap.get(resourceWrapper);
         if (chain == null) {
@@ -206,6 +218,7 @@ public class CtSph implements Sph {
                     }
 
                     // 具体构造chain的方法
+                    // suyh - 生产出责任链，这个责任链我们可以通过spi 扩展接口进行定制。默认是走的DefaultSlotChainBuilder
                     chain = SlotChainProvider.newSlotChain();
                     Map<ResourceWrapper, ProcessorSlotChain> newMap = new HashMap<ResourceWrapper, ProcessorSlotChain>(
                         chainMap.size() + 1);
