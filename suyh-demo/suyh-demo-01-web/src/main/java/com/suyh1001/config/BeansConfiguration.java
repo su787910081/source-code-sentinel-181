bc8f5a21f16f30956dfefeb756ec2c1e00e06e61
su787910081 <su_787910081@sina.com>
2021-07-19 18:51:22 +0800

    Create BeansConfiguration.java

---
+++ suyh-demo/suyh-demo-01-web/src/main/java/com/suyh1001/config/BeansConfiguration.java
@@ -0,0 +1,29 @@
+
+
+package com.suyh1001.config;
+
+import com.alibaba.csp.sentinel.adapter.servlet.CommonFilter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+
+
+@Configuration
+@Slf4j
+public class BeansConfiguration {
+    @Bean
+    public FilterRegistrationBean<Filter> sentinelFilterRegistration() {
+        FilterRegistrationBean<Filter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new CommonFilter());
+        registration.addUrlPatterns("/*");
+        registration.setName("sentinelFilter");
+        registration.setOrder(1);
+
+        log.info("Sentinel servlet CommonFilter registered");
+
+        return registration;
+    }
+}
